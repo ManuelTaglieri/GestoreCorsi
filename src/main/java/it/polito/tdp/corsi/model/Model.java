d2da4a9380bae99d7c4973bb63b43a1829c323ac
Manuel Taglieri <s257476@studenti.polito.it>
2021-03-24 14:31:54 +0100

    Prima parte completata (con anche ordinamento)

--- src/main/java/it/polito/tdp/corsi/model/Model.java
+++ src/main/java/it/polito/tdp/corsi/model/Model.java
@@ -1,7 +1,24 @@
 package it.polito.tdp.corsi.model;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import it.polito.tdp.corsi.db.CorsoDAO;
 
 public class Model {
+
+	private CorsoDAO corsoDao;
 	
+	public Model() {
+		corsoDao = new CorsoDAO();
+	}
 	
+	public List<Corso> getCorsiByPeriodo(Integer id) {
+		return corsoDao.getCorsiByPeriodo(id);
+	}
+	
+	public LinkedHashMap<Corso, Integer> getIscrittiByPeriodo(Integer pd) {
+		return corsoDao.getIscrittibyPeriodo(pd);
+	}
 }
